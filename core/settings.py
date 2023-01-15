c0470421761d83adac161313f57d4947f6e85feb
The-vinicuis <viniciusnao41@gmail.com>
2023-01-15 20:09:46 -0300

    creat directory

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,3 +122,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# directory
+ENCRYPT = BASE_DIR / 'encrypt'
+ZIPS = BASE_DIR / 'zips'
+VIDEOS = BASE_DIR / 'videos'
+
+# verify path exists
+for path in (ENCRYPT, ZIPS, VIDEOS):
+    if not os.path.exists(path):
+        os.mkdir(path)
